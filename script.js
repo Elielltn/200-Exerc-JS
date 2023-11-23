0a91a8878ed36cf244a27cec8e0e71dfd5253b20
Elielltn <126324244+Elielltn@users.noreply.github.com>
2023-11-23 17:48:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,66 @@
+// EXERCICIO 1
+// let idade = 18
+// let peso = 78
+
+// console.log("Idade: ", idade)
+// console.log("Peso: ", peso)
+// EXERCICIO 2
+
+// let pi = 3.14159
+// console.log("PI: ", pi)
+// EXERCICIO 3
+// let numMax = Number.MAX_VALUE
+// console.log("Número Máximo: ", numMax)
+// EXERCICIO 4
+// let nome = "Marcus"
+// let sobrenome = "Silva"
+// let nomeCompleto = nome + " " + sobrenome
+// console.log(nomeCompleto)
+// EXERCICIO 5
+// let num1 = 50
+// let num2 = 80 
+// let soma = num1 + num2;
+// console.log(soma);
+// EXERCICIO 6
+// let falso = false
+// let verdadeiro = true
+
+// console.log(falso && verdadeiro)
+// console.log(verdadeiro && verdadeiro)
+// console.log(falso && falso)
+
+// console.log(verdadeiro || falso)
+// console.log(verdadeiro || verdadeiro)
+// console.log(falso || falso)
+
+// console.log(!verdadeiro)
+// console.log(!falso)
+
+// console.log(!(verdadeiro && falso))
+// console.log(verdadeiro && !falso)
+// EXERCICIO 7
+
+// let num3 = 5
+// let num4 = 9
+// let num5 = 10
+
+// console.log(!(num3 < num4))
+// EXERCICIO 8
+// let num6 = 1
+// console.log(num6)
+// let numIncrement = num6++
+
+// console.log(num6)
+// console.log("Número: ", num6)
+// console.log("Número incrementado: ", numIncrement)
+
+
+//EXERCICIO 9
+function seletor(num) {
+    if (num > 0 ) {
+        console.log("Seu número é positivo")
+    } else if (num < 0){
+        console.log
+    }
+}
+num = prompt("Digite um número")
